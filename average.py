b72f9c85ebacc406a7dde27248620a98c769df2e
SantiEnSriracha <smaldonado@unc.edu.ar>
2023-12-06 16:09:45 -0300

    guardo los EFG un una sola corrida de varias trayectorias

--- average.py
+++ average.py
@@ -11,13 +11,15 @@ read ACF functions and average
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import savgol_filter
-
+from scipy.integrate import cumulative_trapezoid
+#%%
 frame_times = ["500ps", "1ns"]
 runs = ["frames_HQ_1", "frames_HQ_2"]
 
 path = f"../DATA/2023-12_DME/"
 plt.figure(0)
 
+acf = np.zeros((3001,2))
 suma = 0
 for frame_time in frame_times:
   for run in runs:
@@ -27,16 +29,32 @@ for frame_time in frame_times:
     
     
     data = np.loadtxt(filename)
+    for nn in range(2): # uno para cada litio
+        plt.plot(data[:,0], data[:,nn+1])
+        acf[:,nn] = data[:,nn+1]        
     
-    plt.plot(data[:,0], data[:,1])
-    plt.plot(data[:,0], data[:,2])
-    
-    suma += data[:,2] + data[:,1]
+    suma += acf
 
 #%%
-plt.figure(30)
-plt.plot(data[:,0], suma/suma[0], 'k--')
-plt.plot(data[:,0], savgol_filter(suma/suma[0], 500, 3), 'r-', lw=2)
+t = data[:,0]
+plt.figure(1)
+plt.plot(t, suma[:,0], '--', label='Li1')
+plt.plot(t, suma[:,1], '--', label='Li2')
+
+plt.plot(t, savgol_filter(suma[:,0], 500, 3), 'b-', lw=2)
+plt.plot(t, savgol_filter(suma[:,1], 500, 3), 'r-', lw=2)
+
+
+
+s0, s1 = savgol_filter(suma[:,0], 500, 3), savgol_filter(suma[:,1], 500, 3)
+# s0, s1 = suma[:,0], suma[:,1]
+
+integral0, integral1 = cumulative_trapezoid(s0, x=t, initial=0), cumulative_trapezoid(s1, x=t, initial=0)
+
+plt.figure(10)
+plt.plot(t, integral0)
+plt.plot(t, integral1)
+        
 
 #%%
 
@@ -44,7 +62,7 @@ plt.plot(data[:,0], savgol_filter(suma/suma[0], 500, 3), 'r-', lw=2)
 plt.figure(2)
 suma = 0
 ACFS = []
-dt = times[1]-times[0]
+dt = t[1]-t[0]
 for frame_time in frame_times:
   for run in runs:
     
