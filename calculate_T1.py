4ea2dd3e747a61d375df463b3186436c26259837
Santi <santimaldonadoochoa@gmail.com>
2023-12-13 18:49:55 -0300

    T1 tegdme

--- calculate_T1.py
+++ calculate_T1.py
@@ -9,19 +9,24 @@ Calculo T1
 """
 
 
+e = 1.60217663 * 1e-19  # Coulomb
+hbar = 1.054571817 * 1e-34  # joule seconds
+ke = 8.9875517923 * 1e9  # Vm/C, Coulomb constant
 
-e = 1.60217663 * 1e-19 # Coulomb
-hbar = 1.054571817 * 1e-34 #joule seconds
-ke = 8.9875517923 * 1e9# Vm/C, Coulomb constant
-
-Q = -4.01 * (1e-15)**2 # m**2
-I = 1.5 # spin 3/2
+Q = -4.01 * (1e-15)**2  # m**2
+I = 1.5  # spin 3/2
 
 
-gamma = 0.17 # Sternhemmer factor 
+gamma = 0.17  # Sternhemmer factor
 
-tau_c = 10 * 1e-12 # s
-efg_variance = 0.020658403595027623 * ke**2 * e**2 / (1e-10)**6 # (V/m)^2
+
+# DME
+# tau_c = 10 * 1e-12 # s
+# efg_variance = 0.020658403595027623 * ke**2 * e**2 / (1e-10)**6 # (V/m)^2
+
+# TEGDME
+tau_c = 70 * 1e-12  # s
+efg_variance = 0.019666563032557 * ke**2 * e**2 / (1e-10)**6  # (V/m)^2
 
 
 CQ = (2*I+3)*(e*Q/hbar)**2 / (I**2 * (2*I-1)) * (1/20)
