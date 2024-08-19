cf7857281d766feda3c8cab6efbe4a8baa8123c5
SantiEnSriracha <smaldonado@unc.edu.ar>
2024-08-19 17:55:28 -0300

    TO DO LIST en start_DME.py

--- start_DME.py
+++ start_DME.py
@@ -6,6 +6,15 @@ Created on Thu Nov  2 16:33:25 2023
 @author: santi
 
 First script to test the EFG summation
+
+
+TO DO LIST:
+
++ hacer un modulo de funciones.
++ separar el calculo de EFG por especie
++ reemplazar las listas por estructuras mas optimas como np.arrays ¿o dataframes?
+
+
 """
 #%%
 import numpy as np
@@ -111,11 +120,12 @@ species_list = ["Li", "S6", "DME_7CB8A2"]
 frame_times = [f"{t:.1f} ps" for t in [0.5,1,1.5,2]]
 MDfiles = [f"HQ.{i}" for i in range(6,10)]
 
-
+dt = 0.01 # ps (tiempo entre frames)
 
 Charges = get_Charges(species_list)
 #%%
 for idx in range(len(MDfiles)):
+    if idx == 1: continue
     frame_time = frame_times[idx]
     filename = MDfiles[idx]    
 
@@ -125,7 +135,7 @@ for idx in range(len(MDfiles)):
     center = box[0:3]/2
             
     # tiempo en ps
-    trajectory = u.trajectory[:10000:10]
+    trajectory = u.trajectory[:10000:5]
     t = np.zeros(len(trajectory))
     
     EFG = []
@@ -137,9 +147,9 @@ for idx in range(len(MDfiles)):
     for timestep in trajectory:
         nn+=1 
         print("++++++++++++++++++++++++++++++++++++++++++")                    
-        n_frame = u.trajectory.frame
-        t[nn] = u.trajectory.time
-        print(f"dataset {idx}, frame={n_frame}, time = {t[nn]} ps\n\n")                
+        n_frame = u.trajectory.frame        
+        t[nn] = n_frame * dt
+        print(f"dataset {idx}, frame={n_frame}, time = {t[nn]:.2f} ps\n\n")                
 
         group_Li = u.select_atoms("name Li*")    
         Li_pos_t = []
@@ -156,30 +166,20 @@ for idx in range(len(MDfiles)):
                     
                 q = Charges[Charges['AtomType']==AtomType]['Charge'].values[0]
                 
-                group = u.select_atoms(f"name {AtomType}")
-                if 's6t' in AtomType.lower() and nLi==0:                
-                    St_pos_t.append(group.positions)
-                elif 's6i' in AtomType.lower() and nLi==0:                
-                    Si_pos_t.append(group.positions)
-                    
-                
+                group = u.select_atoms(f"name {AtomType}")                                
                 # Calculate distances------------------
                 # Put Li in the center of the universe
                 Li_in_center = Li_atom.position - center
                 # Redefine coordinates with respect to lithium:
-                group_newpositions = group.positions - Li_in_center
-            
+                group_newpositions = group.positions - Li_in_center            
                 # apply PBC to keep the atoms within a unit-cell-length to lithiu
                 group_Cl_newpositions = mda.lib.distances.apply_PBC(group_newpositions,
                                                                     box=box,
-                                                                    backend='openMP')
-                            
-                
+                                                                    backend='openMP')                                            
                 r_distances = mda.lib.distances.distance_array(center, 
-                                                               group_newpositions)
-        
+                                                               group_newpositions)        
                 x_distances, y_distances, z_distances = (group_newpositions-center).T
-        
+
                 if 'li' in AtomType.lower():
                      # Quito la distancia cero, i.e, entre la "autodistancia"
                      x_distances = x_distances[x_distances!=0]
@@ -204,7 +204,7 @@ for idx in range(len(MDfiles)):
     # cada columna de EFG corresponde al litio de group_Li (EN ESE ORDEN)    
     EFG = np.array(EFG)
     # EFG.shape --> (NtimeSteps, NLiAtoms, 3, 3)
-    #%%----------------------------------------------------------
+    #---------------------------------------------------------
     ### EXPORT DATA    
     Vxx, Vyy, Vzz = EFG[:,:,0,0], EFG[:,:,1,1], EFG[:,:,2,2]
     Vxy, Vyz, Vxz = EFG[:,:,0,1], EFG[:,:,1,2], EFG[:,:,0,2]
@@ -219,49 +219,42 @@ for idx in range(len(MDfiles)):
     filename = f"{path}/MDRelax/EFG_{frame_time}.dat"
     np.savetxt(filename, data, header=header)
     #----------------------------------------------------------
-    #%% Calculo el profucto de EFG a tiempo t y a tiempo 0
-    t = t - t[0]
+    #Calculo el profucto de EFG a tiempo t y a tiempo 0
+    # t = t - t[0]
     
-    ACF = np.zeros([t.size, group_Li.n_atoms])
-    for ii in range(group_Li.n_atoms):
-        efg_nLi = EFG[:,ii,:,:]    
-        ACF[:,ii] = np.sum(efg_nLi*efg_nLi[0,:,:], axis=(1,2))
+    # ACF = np.zeros([t.size, group_Li.n_atoms])
+    # for ii in range(group_Li.n_atoms):
+    #     efg_nLi = EFG[:,ii,:,:]    
+    #     ACF[:,ii] = np.sum(efg_nLi*efg_nLi[0,:,:], axis=(1,2))
         
-        plt.figure(0)
-        plt.plot(t, ACF[:,ii]/ACF[0,ii],'o-', label = rf'$Li_{ii+1}$')
-    plt.plot(t, np.mean(ACF, axis=1)/np.mean(ACF, axis=1)[0],'o-', label = r'mean')
-    plt.xlabel("Time [ps]", fontdict={'fontsize':16})
-    plt.ylabel("Autocorrelation Function", fontdict={'fontsize':16})
-    plt.gca().axhline(0, color='k', ls='--')
-    plt.legend()
-    
-    
-    plt.show()
+    #     plt.figure(0)
+    #     plt.plot(t, ACF[:,ii]/ACF[0,ii],'o-', label = rf'$Li_{ii+1}$')
+    # plt.plot(t, np.mean(ACF, axis=1)/np.mean(ACF, axis=1)[0],'o-', label = r'mean')
+    # plt.xlabel("Time [ps]", fontdict={'fontsize':16})
+    # plt.ylabel("Autocorrelation Function", fontdict={'fontsize':16})
+    # plt.gca().axhline(0, color='k', ls='--')
+    # plt.legend()
+    # plt.show()
     
     #---------------
-    #%% Calculo el profucto de EFG a tiempo t y a tiempo 0,
+    #Calculo el profucto de EFG a tiempo t y a tiempo 0,
     ### esta vez variando cual es el tiempo 0 (promedio en ensamble)
-    
-
-
-
-    #### CORREGIR ESTO!!!!!
-
+    dtau = np.diff(t)[0]
     efg = EFG
     acf = np.zeros([t.size, group_Li.n_atoms])
     Num_promedios = np.zeros(t.size)
     for ii in range(t.size):    
-        tau = ii
+        tau = ii*dtau
         jj, t0, acf_ii = 0, 0, 0
-        while t0+tau<=times[-1]:
-            print(f"tau = {tau} fs, t0 = {t0} ps, ---------{jj}")                
+        while t0+tau<=t[-1]:
+            print(f"tau = {tau} ps, t0 = {t0:.2f} ps, ---------{jj}")                
             acf_ii += np.sum(efg[ii,:,:,:]*efg[ii+jj,:,:,:], axis=(1,2))
             jj+=1
-            t0 = jj
+            t0 = jj*dtau
         print(f"el promedio es dividir por {jj}")
         acf[ii,:] = acf_ii/jj
         Num_promedios[ii] = jj
-    #%%
+    #
     plt.figure(44847)
     plt.plot(t, Num_promedios,'k--')
     plt.xlabel("Time [ps]", fontdict={'fontsize':16})
@@ -284,7 +277,7 @@ for idx in range(len(MDfiles)):
     plt.ylabel(r"$\langle\ EFG(t)\cdot EFG(0)\ \rangle_t$",
                fontdict={'fontsize':16})
     
-    # plt.xlim([0,3])
+    plt.xlim([0,t[-1]*0.5])
     plt.gca().axhline(0, color='k', ls='--')
     # plt.gca().axhline(1, color='k', ls='--')
     plt.tight_layout()
@@ -292,7 +285,7 @@ for idx in range(len(MDfiles)):
     plt.savefig(f"{path}/MDRelax/ACF.png")
     plt.show()
     
-    #%%
+    #
     plt.figure(44849)
     plt.plot(t, np.mean(acf, axis=1)/np.mean(acf, axis=1)[0],'o-', color='green', 
              label = r'Promedio entre atomos de Li y en $<>_t$')
@@ -303,6 +296,4 @@ for idx in range(len(MDfiles)):
     plt.tight_layout()
     plt.legend()
     plt.savefig(f"{path}/MDRelax/ACFnorm.png")
-    plt.show()
-    
-    
+    plt.show()    
