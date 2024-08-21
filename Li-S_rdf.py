5f210fea1e9f90d2e369fc55f0ae491aae4922ef
SantiEnSriracha <smaldonado@unc.edu.ar>
2024-08-21 13:49:42 -0300

    closer Li

--- Li-S_rdf.py
+++ Li-S_rdf.py
@@ -34,20 +34,46 @@ for idx in range(len(MDfiles)):
     for Li_resid in Li_resids:
         ii+=1        
         rdf = InterRDF(u.select_atoms(f"resid {Li_resid}"), u.select_atoms("name S6t"))
+        label = f"run: {run}, Li resid {Li_resid}"
         rdf.run()
         plt.figure(ii)
-        plt.plot(rdf.bins, rdf.rdf, 'o-', label=f"run: {run}, Li resid {Li_resid}")
+        plt.plot(rdf.bins, rdf.rdf, 'o-', label=label)
         plt.legend()
         plt.figure(ii*10)
-        plt.plot(rdf.bins, np.gradient(rdf.rdf), 'o-', label=f"run: {run}, Li resid {Li_resid}")
-        rdfs.append([rdf.bins, rdf.rdf])
+        plt.plot(rdf.bins, np.gradient(rdf.rdf), 'o-', label=label)
+        rdfs.append([rdf.bins, rdf.rdf, label])
         plt.legend()
 
-plt.figure(0)
+#%%
 suma = 0
 for rdf in rdfs:
-    plt.plot(rdf[0], rdf[1], 'o-')
-    suma+=rdf[1]
-plt.figure(5454354354354)
-plt.plot(rdfs[0][0], suma, 'o-')
+    ii=ii%2    
+    if ii==0:
+        rdf_ii = np.zeros([len(rdf[0]), 2])
+        derivadas = np.zeros([len(rdf[0]), 2])
+        labels = []
+    derivadaSegunda = np.gradient(np.gradient(rdf[1]))
+    derivadas[:, ii] = derivadaSegunda
+    rdf_ii[:, ii] = rdf[1]
+    labels.append(rdf[2])    
+    if ii==1:
+        derivadas_range = derivadas[np.abs(rdf[0]-3)<1]
+        rdf_close = np.argmin(np.min(derivadas_range, axis=0))
+        rdf_far = np.argmax(np.min(derivadas_range, axis=0))        
+        plt.figure()
+        plt.plot(rdf[0], rdf_ii[:, rdf_close], 'ro-', label = f"closer Li==> {labels[rdf_close]}")
+        plt.plot(rdf[0], rdf_ii[:, rdf_far], 'bo-', label = f"far Li==> {labels[rdf_far]}")    
+        plt.legend()
 
+        # plt.figure()
+        # plt.plot(rdf[0], derivadas[:, rdf_close], 'ro--', label = f"closer Li==> {labels[rdf_close]}")
+        # plt.plot(rdf[0], derivadas[:, rdf_far], 'bo--', label = f"far Li==> {labels[rdf_far]}")    
+        # plt.legend(title="Derivadas Segundas")
+    ii+=1    
+
+#     suma+=rdf[1]
+# plt.figure(5454354354354)
+# plt.plot(rdfs[0][0], suma, 'o-')
+
+
+# %%
