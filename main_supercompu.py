39971919be97cf65679e7b40d86e7812d78ece67
santimaldo <santimaldonadoochoa@gmail.com>
2024-10-28 13:19:15 -0300

    superc

--- main_supercompu.py
+++ main_supercompu.py
@@ -27,7 +27,7 @@ cation, anion, solvent = ["Li","none", "SOL"] # names
 # salt = r"Li$^+$"
 salt = r"Li$^+$"
 Ncations = 1 # numero de Li+
-runs_inds = range(6,7)
+runs_inds = range(7,11)
 runs_prefix = "HQ"
 mdp_prefix = "HQ.long"
 runs_suffix = [f".{t*1000:.0f}_ps.long" for t in runs_inds]
@@ -38,21 +38,28 @@ topology_format = ".gro" # ".tpr" or ".gro"
 forcefield = "Madrid.ff"
 
 t0 = time.time()
-# get_EFG_data(path_Gromacs, path_MDrelax,
-#              species = [cation, anion, solvent],
-#              species_itp = [cation_itp, anion_itp, solvent_itp],
-#              Ncations = Ncations,
-#              runs_prefix = runs_prefix,
-#              runs_suffix = runs_suffix,
-#              runs_suffix_gro = runs_suffix_gro,
-#              trajectory_format = trajectory_format,
-#              topology_format = topology_format,
-#              forcefield=forcefield,
-#              mdp_prefix=mdp_prefix
-#              )
-# print(f"EFG time: {time.time()-t0:.0f} s")
+get_EFG_data(path_Gromacs, path_MDrelax,
+             species = [cation, anion, solvent],
+             species_itp = [cation_itp, anion_itp, solvent_itp],
+             Ncations = Ncations,
+             runs_prefix = runs_prefix,
+             runs_suffix = runs_suffix,
+             runs_suffix_gro = runs_suffix_gro,
+             trajectory_format = trajectory_format,
+             topology_format = topology_format,
+             forcefield=forcefield,
+             mdp_prefix=mdp_prefix
+             )
+print(f"EFG time: {time.time()-t0:.0f} s")
 
 #%%
+
+runs_inds = range(6,11)
+runs_prefix = "HQ"
+mdp_prefix = "HQ.long"
+runs_suffix = [f".{t*1000:.0f}_ps.long" for t in runs_inds]
+runs_suffix_gro = [f".{t:.0f}.long" for t in runs_inds]
+
 
 t0 = time.time()
 calculate_ACF(path_MDrelax,
