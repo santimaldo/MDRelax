29231d41ccbce8b979ec8553112d13bb674b47cf
SantiEnSriracha <smaldonado@unc.edu.ar>
2024-10-31 10:26:14 -0300

    update

--- ACF_comparison.py
+++ ACF_comparison.py
@@ -14,36 +14,25 @@ import numpy as np
 from Functions import cumulative_simpson
 plt.rcParams.update({'font.size': 16})
 
-# Define the weighted sum of two exponential decays
-def ExpDecN(x, a1, tau1, a2, tau2):
-    return a1 * np.exp(-x / tau1) + a2 * np.exp(-x / tau2) 
-
-# Define the weighted sum of N exponential decays
-def ExpDecN(x, *params):
-    y = np.zeros_like(x)
-    N = len(params) // 2
-    for i in range(N):
-        a = params[2*i]
-        tau = params[2*i + 1]
-        y += a * np.exp(-x / tau)
-    return y
-
 
 names=[]
 paths=[]
 # ACF 0
-names.append("Li+water_guardado-cada-0.001ps")
+# names.append("Li+water_guardado-cada-0.001ps")
+# paths.append("/home/santi/MD/MDRelax_results/Li-water/long/")
+
+# # ACF 1
+# names.append("Li+water_guardado-cada-0.01ps")
+# paths.append("/home/santi/MD/MDRelax_results/Li-water/freq0.1/")
+
+# ACF 0
+names.append(r"Water-$Li^+$")
 paths.append("/home/santi/MD/MDRelax_results/Li-water/long/")
-
-# ACF 1
-names.append("Li+water_guardado-cada-0.01ps")
-paths.append("/home/santi/MD/MDRelax_results/Li-water/freq0.1/")
-
 # ACF 2
-# names.append(r"DME-$Li^+$")
-# paths.append("/home/santi/MD/MDRelax_results/DME_no-anion/")
+names.append(r"DME-$Li^+$")
+paths.append("/home/santi/MD/MDRelax_results/DME_small-boxes/DME_no-anion/")
 
-cutoff_time = 10 # ps
+cutoff_time = 100 # ps
 fig, axs= plt.subplots(nrows=2, ncols=1,figsize=(10, 10))
 # fig, (ax, ax_resid) = plt.subplots(nrows=2, ncols=1, figsize=(10, 8), gridspec_kw={'height_ratios': [3, 1]})
 for idx, (path, name) in enumerate(zip(paths, names)):
@@ -73,6 +62,7 @@ for idx, (path, name) in enumerate(zip(paths, names)):
     ax.legend()    
     ax.set_ylabel(r"$\int_0^{\tau} ACF(t') dt'$  [ps]", fontsize=16)
     ax.set_xlabel(r"$\tau$ [ps]", fontsize=16)    
-
+ax.set_yscale('log')
+ax.set_ylim(0.01,10)
 fig.suptitle(fr"EFG Autocorrelation Function", fontsize=16)
 fig.tight_layout()
