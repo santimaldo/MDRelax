0a772ad453f7a984111d986ab5ab799dff24cc2e
SantiEnSriracha <smaldonado@unc.edu.ar>
2024-11-05 18:02:08 -0300

    jugando con 1H

--- old/start_1H_DME.py
+++ old/start_1H_DME.py
@@ -17,19 +17,28 @@ import nmrformd as nmrmd
 import time
 from scipy.integrate import cumulative_trapezoid
 
-path = "../DATA/2023-12_DME/500ps/frames_HQ_2/"
-savepath = "../DATA/2023-12_DME/results/1H/"
-u = mda.Universe(path+"HQ_npt-500ps_2.tpr", path+"HQ_npt-500ps_2.xtc")
+# path = "../DATA/2023-12_DME/500ps/frames_HQ_2/"
+# savepath = "../DATA/2023-12_DME/results/1H/"
+# u = mda.Universe(path+"HQ_npt-500ps_2.tpr", path+"HQ_npt-500ps_2.xtc")
 
 #path = "../DATA/2023-12_TEGDME/500ps/frames_HQ_1/"
 #u = mda.Universe(path+"HQ_npt-500ps_1.tpr", path+"HQ_npt-500ps_1.xtc")
 
+
+# path = "/home/santi/mendieta/DME_small-boxes/DME_PS/run_1ns/"
+# savepath = "/home/santi/tmp/"
+# u = mda.Universe(path+"HQ.6.tpr", path+"HQ.6.xtc")
+
+
+file = "/home/santi/mendieta/Li-water/HQ.8.long.freq0.1"
+savepath = "/home/santi/tmp/"
+u = mda.Universe(file+".tpr", file+".xtc")
+
 dt = u.trajectory.dt # units of ps
 ni = 40 # "number_i"
-start_time = time.time()
 box = u.dimensions
 
-
+print(f"file {file}: check ")
 H_group = u.select_atoms("name H*")
 Li_group = u.select_atoms("name Li*")
 
@@ -47,62 +56,62 @@ H_bond = u.select_atoms('index ' + ' '.join(map(str, H_group_within_threshold.in
 print("Number of selected H_bond atoms:", len(H_bond))
 #---------------------------------------------------------------------
 H_free = H_group.difference(H_bond)
-n_i = len(H_bond)
-
-print("Comenzazmos con H_bond")
-print("calculando intra...") #(si no es intra, es inter_molecular)
-nmr_H_bond_intra = nmrmd.NMR(u, atom_group=H_bond, isotropic=True, #actual_dt=dt, 
-                            type_analysis="intra_molecular", neighbor_group=H_group)
-print("calculando inter...")
-nmr_H_bond_inter= nmrmd.NMR(u, atom_group=H_bond, isotropic=True, #actual_dt=dt,
-                         type_analysis="inter_molecular", neighbor_group=H_group)
-print("calculando total...")
-nmr_H_bond= nmrmd.NMR(u, atom_group=H_bond, isotropic=True, #actual_dt=dt
-                        neighbor_group=H_group)
-elapsed_time = time.time() - end_time
-end_time = time.time()
+# n_i = len(H_bond)
+# start_time = time.time()
+# print("Comenzazmos con H_bond")
+# print("calculando intra...") #(si no es intra, es inter_molecular)
+# nmr_H_bond_intra = nmrmd.NMR(u, atom_group=H_bond, isotropic=True, #actual_dt=dt, 
+#                             type_analysis="intra_molecular", neighbor_group=H_group)
+# print("calculando inter...")
+# nmr_H_bond_inter= nmrmd.NMR(u, atom_group=H_bond, isotropic=True, #actual_dt=dt,
+#                          type_analysis="inter_molecular", neighbor_group=H_group)
+# print("calculando total...")
+# nmr_H_bond= nmrmd.NMR(u, atom_group=H_bond, isotropic=True, #actual_dt=dt
+#                         neighbor_group=H_group)
+# elapsed_time = time.time() - start_time
+# print(f'Time elapsed: {elapsed_time/60} minutes')
 #---------------------------------------------------------------
+start_time = time.time()
 print("Continuamos con H_free")
-print("calculando intra...") #(si no es intra, es inter_molecular)
-nmr_H_free_intra = nmrmd.NMR(u, atom_group=H_free, isotropic=True,# actual_dt=dt, 
-                            number_i=ni, type_analysis="intra_molecular", neighbor_group=H_group)
-print("calculando inter...")
-nmr_H_free_inter = nmrmd.NMR(u, atom_group=H_free, isotropic=True,# actual_dt=dt, 
-                            number_i=ni, type_analysis="inter_molecular", neighbor_group=H_group)
+# print("calculando intra...") #(si no es intra, es inter_molecular)
+# nmr_H_free_intra = nmrmd.NMR(u, atom_group=H_free, isotropic=True,# actual_dt=dt, 
+#                             number_i=ni, type_analysis="intra_molecular", neighbor_group=H_group)
+# print("calculando inter...")
+# nmr_H_free_inter = nmrmd.NMR(u, atom_group=H_free, isotropic=True,# actual_dt=dt, 
+#                             number_i=ni, type_analysis="inter_molecular", neighbor_group=H_group)
 print("calculando total...")
 nmr_H_free = nmrmd.NMR(u, atom_group=H_free, isotropic=True, #actual_dt=dt, 
                         number_i=ni, neighbor_group=H_group)
-elapsed_time = time.time() - end_time
-end_time = time.time()
+elapsed_time = time.time() - start_time
 print(f'Time elapsed: {elapsed_time/60} minutes')
 
 
-#%%
-R1dispersion_bond_intra = nmr_H_bond_intra.R1
-R1dispersion_bond_inter = nmr_H_bond_inter.R1
-R1dispersion_bond = nmr_H_bond.R1
-R1dispersion_free_intra = nmr_H_free_intra.R1
-R1dispersion_free_inter = nmr_H_free_inter.R1
+#
+# R1dispersion_bond_intra = nmr_H_bond_intra.R1
+# R1dispersion_bond_inter = nmr_H_bond_inter.R1
+# R1dispersion_bond = nmr_H_bond.R1
+# R1dispersion_free_intra = nmr_H_free_intra.R1
+# R1dispersion_free_inter = nmr_H_free_inter.R1
 R1dispersion_free = nmr_H_free.R1
 
 
-R2dispersion_bond_intra = nmr_H_bond_intra.R2
-R2dispersion_bond_inter = nmr_H_bond_inter.R2
-R2dispersion_bond = nmr_H_bond.R2
-R2dispersion_free_intra = nmr_H_free_intra.R2
-R2dispersion_free_inter = nmr_H_free_inter.R2
+# R2dispersion_bond_intra = nmr_H_bond_intra.R2
+# R2dispersion_bond_inter = nmr_H_bond_inter.R2
+# R2dispersion_bond = nmr_H_bond.R2
+# R2dispersion_free_intra = nmr_H_free_intra.R2
+# R2dispersion_free_inter = nmr_H_free_inter.R2
 R2dispersion_free = nmr_H_free.R2
-frequency_bond = nmr_H_bond.f
+# frequency_bond = nmr_H_bond.f
 frequency_free = nmr_H_free.f
 
-ACF_bond_intra = nmr_H_bond_intra.gij[0,:]
-ACF_bond_inter = nmr_H_bond_inter.gij[0,:]
-ACF_bond = nmr_H_bond.gij[0,:]
-ACF_free_intra = nmr_H_free_intra.gij[0,:]
-ACF_free_inter = nmr_H_free_inter.gij[0,:]
+# ACF_bond_intra = nmr_H_bond_intra.gij[0,:]
+# ACF_bond_inter = nmr_H_bond_inter.gij[0,:]
+# ACF_bond = nmr_H_bond.gij[0,:]
+# ACF_free_intra = nmr_H_free_intra.gij[0,:]
+# ACF_free_inter = nmr_H_free_inter.gij[0,:]
 ACF_free = nmr_H_free.gij[0,:]
-
-tau = np.arange(ACF_bond.size)*dt
+#
+tau = np.arange(ACF_free.size)*dt
 
 # # data = np.array([tau, ACF, ACF_intra, ACF_inter]).T
 # #%%
@@ -116,13 +125,13 @@ tau = np.arange(ACF_bond.size)*dt
 
 
 
-
+#%%
 plt.figure(1)
-plt.loglog(frequency_bond, 1/R1dispersion_bond_intra, 'o-', label="H_bond_intra")
-plt.loglog(frequency_bond, 1/R1dispersion_bond_inter, 'o-', label="H_bond_inter")
-plt.loglog(frequency_bond, 1/R1dispersion_bond, 'o-', label="H_bond")
-plt.loglog(frequency_free, 1/R1dispersion_free_intra, 'o-', label="H_free_intra")
-plt.loglog(frequency_free, 1/R1dispersion_free_inter, 'o-', label="H_free_inter")
+# plt.loglog(frequency_bond, 1/R1dispersion_bond_intra, 'o-', label="H_bond_intra")
+# plt.loglog(frequency_bond, 1/R1dispersion_bond_inter, 'o-', label="H_bond_inter")
+# plt.loglog(frequency_bond, 1/R1dispersion_bond, 'o-', label="H_bond")
+# plt.loglog(frequency_free, 1/R1dispersion_free_intra, 'o-', label="H_free_intra")
+# plt.loglog(frequency_free, 1/R1dispersion_free_inter, 'o-', label="H_free_inter")
 plt.loglog(frequency_free, 1/R1dispersion_free, 'o-', label="H_free")
 plt.legend()
 plt.xlabel("frequency [Hz]")
@@ -130,23 +139,24 @@ plt.ylabel("T1 [s]")
 
 
 plt.figure(2)
-plt.loglog(frequency_bond, 1/R2dispersion_bond_intra, 'o-', label="H_bond_intra")
-plt.loglog(frequency_bond, 1/R2dispersion_bond_inter, 'o-', label="H_bond_inter")
-plt.loglog(frequency_bond, 1/R2dispersion_bond, 'o-', label="H_bond")
-plt.loglog(frequency_free, 1/R2dispersion_free_intra, 'o-', label="H_free_intra")
-plt.loglog(frequency_free, 1/R2dispersion_free_inter, 'o-', label="H_free_inter")
+# plt.loglog(frequency_bond, 1/R2dispersion_bond_intra, 'o-', label="H_bond_intra")
+# plt.loglog(frequency_bond, 1/R2dispersion_bond_inter, 'o-', label="H_bond_inter")
+# plt.loglog(frequency_bond, 1/R2dispersion_bond, 'o-', label="H_bond")
+# plt.loglog(frequency_free, 1/R2dispersion_free_intra, 'o-', label="H_free_intra")
+# plt.loglog(frequency_free, 1/R2dispersion_free_inter, 'o-', label="H_free_inter")
 plt.loglog(frequency_free, 1/R2dispersion_free, 'o-', label="H_free")
 plt.legend()
 plt.xlabel("frequency [Hz]")
 plt.ylabel("T2 [s]")
 
 plt.figure(3)
-plt.plot(np.arange(ACF_bond.size)*u.trajectory.dt, ACF_bond_intra.T, 'o-', label="H_bond_intra")
-plt.plot(np.arange(ACF_bond.size)*u.trajectory.dt, ACF_bond_inter.T, 'o-', label="H_bond_inter")
-plt.plot(np.arange(ACF_bond.size)*u.trajectory.dt, ACF_bond.T, 'o-', label="H_bond")
-plt.plot(np.arange(ACF_free.size)*u.trajectory.dt, ACF_free_intra.T, 'o-', label="H_free_intra")
-plt.plot(np.arange(ACF_free.size)*u.trajectory.dt, ACF_free_inter.T, 'o-', label="H_free_inter")
+# plt.plot(np.arange(ACF_bond.size)*u.trajectory.dt, ACF_bond_intra.T, 'o-', label="H_bond_intra")
+# plt.plot(np.arange(ACF_bond.size)*u.trajectory.dt, ACF_bond_inter.T, 'o-', label="H_bond_inter")
+# plt.plot(np.arange(ACF_bond.size)*u.trajectory.dt, ACF_bond.T, 'o-', label="H_bond")
+# plt.plot(np.arange(ACF_free.size)*u.trajectory.dt, ACF_free_intra.T, 'o-', label="H_free_intra")
+# plt.plot(np.arange(ACF_free.size)*u.trajectory.dt, ACF_free_inter.T, 'o-', label="H_free_inter")
 plt.plot(np.arange(ACF_free.size)*u.trajectory.dt, ACF_free.T, 'o-', label="H_free")
+plt.axhline(0, linestyle='--', color='k')
 plt.legend()
 plt.xlabel(r"$\tau$ [ps]")
 plt.ylabel("ACF")
@@ -154,12 +164,13 @@ plt.show()
 
 
 plt.figure(4)
-plt.plot(np.arange(ACF_bond.size)*u.trajectory.dt, ACF_bond_intra.T/ACF_bond_intra[0], 'o-', label="H_bond_intra")
-plt.plot(np.arange(ACF_bond.size)*u.trajectory.dt, ACF_bond_inter.T/ACF_bond_inter[0], 'o-', label="H_bond_inter")
-plt.plot(np.arange(ACF_bond.size)*u.trajectory.dt, ACF_bond.T/ACF_bond[0], 'o-', label="H_bond")
-plt.plot(np.arange(ACF_free.size)*u.trajectory.dt, ACF_free_intra.T/ACF_free_intra[0], 'o-', label="H_free_intra")
-plt.plot(np.arange(ACF_free.size)*u.trajectory.dt, ACF_free_inter.T/ACF_free_inter[0], 'o-', label="H_free_inter")
+# plt.plot(np.arange(ACF_bond.size)*u.trajectory.dt, ACF_bond_intra.T/ACF_bond_intra[0], 'o-', label="H_bond_intra")
+# plt.plot(np.arange(ACF_bond.size)*u.trajectory.dt, ACF_bond_inter.T/ACF_bond_inter[0], 'o-', label="H_bond_inter")
+# plt.plot(np.arange(ACF_bond.size)*u.trajectory.dt, ACF_bond.T/ACF_bond[0], 'o-', label="H_bond")
+# plt.plot(np.arange(ACF_free.size)*u.trajectory.dt, ACF_free_intra.T/ACF_free_intra[0], 'o-', label="H_free_intra")
+# plt.plot(np.arange(ACF_free.size)*u.trajectory.dt, ACF_free_inter.T/ACF_free_inter[0], 'o-', label="H_free_inter")
 plt.plot(np.arange(ACF_free.size)*u.trajectory.dt, ACF_free.T/ACF_free[0], 'o-', label="H_free")
+plt.axhline(0, linestyle='--', color='k')
 plt.legend()
 plt.xlabel(r"$\tau$ [ps]")
 plt.ylabel(r"$ACF_{NORM}$")
