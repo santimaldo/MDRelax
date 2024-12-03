2261ed9349a99baae77df61c8c2fb99dfdba8861
SantiEnSriracha <smaldonado@unc.edu.ar>
2024-12-03 11:26:46 -0300

    creacion del codigo para calcular rmsd

---
+++ rmsd.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 3 2024
+
+@author: santi
+
+read MD data and calculate RMSD
+"""
+import time
+import MDAnalysis as mda
+import MDAnalysis.analysis.rms as rms
+import matplotlib.pyplot as plt
+import numpy as np
+
+path = "/home/santi/mendieta/Diglyme_small-boxes/Diglyme_LiTFSI/run_1ns/"
+file = "NPT"
+
+solvent = "DIG"
+cation = "Lij"
+anion = "TFS"
+
+trajectory_extension = ".trr"
+# defino para quien calculo:
+selected = cation
+
+
+u = mda.Universe(path+file+".tpr", path+file+trajectory_extension)
+
+selection_str = f"resname {selected}"
+selected_atoms = u.select_atoms(selection_str)
+t0 = time.time()                
+R = rms.RMSD(u,
+             select=selection_str
+             )
+R.run()
+tf = time.time()
+print(f"tiempo de ejecucion:   {tf-t0} s") 
+
+#%%
+rmsd = R.results.rmsd   # transpose makes it easier for plotting
+header = "index\ttime[ps]\tRMSD[Ang]\n"
+header += f"based on {selected_atoms.n_atoms} atoms from {selected} group"
+np.savetxt(path+file+f"_{selected}-RMSD.dat", rmsd, header=header)
+
+time = rmsd[:,1]
+
+fig = plt.figure(figsize=(4,4))
+ax = fig.add_subplot(111)
+ax.plot(time/1000, rmsd[:,2], label=selected)
+ax.legend(loc="best")
+ax.set_xlabel("time (ns)")
+ax.set_ylabel(r"RMSD ($\AA$)")
+# fig.savefig("rmsd_all_CORE_LID_NMP_ref1AKE.pdf")
+# %%
