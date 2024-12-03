b4cdc69ed0af20718b9b3389a700b754b926ab82
SantiEnSriracha <smaldonado@unc.edu.ar>
2024-12-03 13:09:12 -0300

    actualizo con promedio en tiempo inicial

--- rmsd.py
+++ rmsd.py
@@ -13,42 +13,82 @@ import MDAnalysis.analysis.rms as rms
 import matplotlib.pyplot as plt
 import numpy as np
 
-path = "C:\Users\Usuario\Documents\SantiM\MDdata\mendieta\Diffusion\Diglyme_PS/"
-savepath = "C:\Users\Usuario\Documents\SantiM\MDdata\MDrelax_results\Diffusion\Diglyme_PS/"
-file = "npt_diff"
+# path = "C:\Users\Usuario\Documents\SantiM\MDdata\mendieta\Diffusion\Diglyme_PS/"
+# savepath = "C:\Users\Usuario\Documents\SantiM\MDdata\MDrelax_results\Diffusion\Diglyme_PS/"
+# file = "npt_diff"
+# trajectory_extension = ".xtc"
+
+path = "/home/santi/mendieta/Diglyme_small-boxes/Diglyme_LiTFSI/run_1ns/"
+savepath = path
+file = "NPT"
+trajectory_extension = ".trr"
 
 solvent = "DIG"
 cation = "Lij"
 anion = "TFS"
-
-trajectory_extension = ".xtc"
 # defino para quien calculo:
 selected = cation
 
 
 u = mda.Universe(path+file+".tpr", path+file+trajectory_extension)
 
+# Selección de átomos
 selection_str = f"resname {selected}"
 selected_atoms = u.select_atoms(selection_str)
-t0 = time.time()                
-R = rms.RMSD(u,
-             select=selection_str
-             )
-R.run()
+# Listas para almacenar los resultados
+Ntimes = len(u.trajectory)
+rmsd_results = np.zeros(Ntimes)
+ti = time.time()                
+# Iterar sobre los frames de referencia (t0)
+np.arange(0,500001,5000)
+for t0 in t0s: 
+    u.trajectory[t0]  # Mover el universo al frame t0 (fijar referencia)
+
+    # Configurar RMSD con el frame de referencia actual (t0)
+    R = rms.RMSD(u,
+                 select=selection_str,
+                 ref_frame=t0  # Frame de referencia
+                 )
+    R.run()
+
+    if t0==0:
+        times = R.rmsd[:, 1]
+        tf = time.time()
+        print(f"tiempo de ejecucion (ref frame 0):   {tf-ti} s") 
+
+    # Filtrar solo tiempos posteriores
+    rmsd_post_t0 = R.rmsd[t0:, 2]  # RMSD (Ångstroms) para frames después de t0
+    
+    rmsd_results[0:len(rmsd_post_t0)] += rmsd_post_t0
+    
+# calculo el promedio de RMSD solo para el caso en que tienen la misma cantidad de frames    
+last_time = Ntimes - t0s[-1]
+rmsd = rmsd_results[:last_time]/len(t0s)
+times = times[:last_time]
+# Output de los resultados
 tf = time.time()
-print(f"tiempo de ejecucion:   {tf-t0} s") 
+print(f"tiempo de ejecucion total:   {tf-ti} s") 
+
+# selection_str = f"resname {selected}"
+# selected_atoms = u.select_atoms(selection_str)
+# t0 = time.time()                
+# R = rms.RMSD(u,
+#              select=selection_str
+#              )
+# R.run()
+# tf = time.time()
+# print(f"tiempo de ejecucion:   {tf-t0} s") 
 
 #%%
-rmsd = R.results.rmsd   # transpose makes it easier for plotting
-header = "index\ttime[ps]\tRMSD[Ang]\n"
-header += f"based on {selected_atoms.n_atoms} atoms from {selected} group"
-np.savetxt(savepath+file+f"_{selected}-RMSD.dat", rmsd, header=header)
-
-time = rmsd[:,1]
+# rmsd = R.results.rmsd   # transpose makes it easier for plotting
+# header = "index\ttime[ps]\tRMSD[Ang]\n"
+# header += f"based on {selected_atoms.n_atoms} atoms from {selected} group"
+# np.savetxt(savepath+file+f"_{selected}-RMSD.dat", rmsd, header=header)
+# time = rmsd[:,1]
 
 fig = plt.figure(figsize=(4,4))
 ax = fig.add_subplot(111)
-ax.plot(time/1000, rmsd[:,2], label=selected)
+ax.plot(times/1000, rmsd, label=selected)
 ax.legend(loc="best")
 ax.set_xlabel("time (ns)")
 ax.set_ylabel(r"RMSD ($\AA$)")
